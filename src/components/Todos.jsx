d2e9e070346be19bc69705fa3b59a5b7b2e4cef3
ahsanaaxioned <ahsana@axioned.com>
2023-08-14 00:54:36 +0530

    hover styling updated and partially event added

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 
 const Todos = ()=> {
+  const submitEvent = (e)=>{
+    e.preventDefault();
+  }
+  const todosHandler = (e)=>{ 
+    if (e.key === 'Enter') {
+      console.log(e.target.value);
+    }
+  }
+
   return (
    <div className ='w-[70%] mx-auto bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
-     <form>
+     <form onSubmit={submitEvent }>
       <div>
-        <input type="text" placeholder='What needs to be done?' name='search' className='common outline-none' />
+        <input type="text" placeholder='What needs to be done?' name='search' className='common outline-none' onKeyDown={todosHandler} />
       </div>
     </form>
     <div>
         <ul>
-            <li className='common relative icon'>ashjvgf jgczxf</li>
+            <li className='common relative icon completed'>ashjvgf jgczxf</li>
         </ul>
         <div className='flex justify-between px-[15px] items-center'>
             <span>1 item left</span>
