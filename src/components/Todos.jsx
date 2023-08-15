cdd1c05c4366465caa474c30bb31812748f2e0d1
ahsanaaxioned <ahsana@axioned.com>
2023-08-16 00:21:44 +0530

    Added: Partially logic added

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,37 +1,76 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-const Todos = ()=> {
-  const submitEvent = (e)=>{
+const Todos = () => {
+  const [inputVal, setInputVal] = useState(""),
+    [todostask, setTodosTask] = useState([]),
+    [empty, setEmpty] = useState(false);
+
+  const submitEvent = (e) => {
     e.preventDefault();
-  }
-  const todosHandler = (e)=>{ 
-    if (e.key === 'Enter') {
-      console.log(e.target.value);
+  };
+
+  const todosHandler = (e) => {
+    if (e.key === "Enter") {
+      if (inputVal !== "") {
+        setTodosTask([...todostask, inputVal]);
+        setInputVal("");
+        setEmpty(false);
+      } else {
+        setEmpty(true);
+      }
     }
-  }
+  };
+
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem("todosItems"));
+    if (storedTodos) {
+      setTodosTask(storedTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("todosItems", JSON.stringify(todostask));
+  }, [todostask]);
 
   return (
-   <div className ='w-[70%] mx-auto bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]'>
-     <form onSubmit={submitEvent }>
-      <div>
-        <input type="text" placeholder='What needs to be done?' name='search' className='common outline-none' onKeyDown={todosHandler} />
-      </div>
-    </form>
-    <div>
-        <ul>
-            <li className='common relative icon completed'>ashjvgf jgczxf</li>
-        </ul>
-        <div className='flex justify-between px-[15px] items-center'>
-            <span>1 item left</span>
-            <ul className='flex justify-between py-3 basis-[45%] text-black gap-2'>
-                <li className='basis-[20%] border-[1px] border-[#EFD5D5] block text-center'>All</li>
-                <li className='basis-[38%] block text-center'>Active</li>
-                <li className='basis-[38%] block text-center'>Completed</li>
-            </ul>
-            <button>Clear completed</button>
+    <div className="w-[70%] mx-auto bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+      <form onSubmit={submitEvent}>
+        <div>
+          <input
+            type="text"
+            placeholder={empty ? "Field is required" : "What needs to be done?"}
+            name="search"
+            value={inputVal}
+            className={`${
+              empty ? "placeholder:text-red-700" : "placeholder:text-[#989898]"
+            } common outline-none`}
+            onKeyDown={todosHandler}
+            onChange={(e) => setInputVal(e.target.value)}
+          />
         </div>
+      </form>
+      <div>
+        <ul>
+          {todostask.map((value, idx) => (
+            <li key={idx} className="common relative icon completed">
+              {value}
+            </li>
+          ))}
+        </ul>
+        <div className="flex justify-between px-[15px] items-center">
+          <span>{todostask.length} item{todostask.length !== 1 ? "s" : ""} left</span>
+          <ul className="flex justify-between py-3 basis-[45%] text-black gap-2">
+            <li className="basis-[20%] border-[1px] border-[#EFD5D5] block text-center">
+              All
+            </li>
+            <li className="basis-[38%] block text-center">Active</li>
+            <li className="basis-[38%] block text-center">Completed</li>
+          </ul>
+          <button>Clear completed</button>
+        </div>
+      </div>
     </div>
-   </div>
-  )
-}
-export default Todos
+  );
+};
+
+export default Todos;
