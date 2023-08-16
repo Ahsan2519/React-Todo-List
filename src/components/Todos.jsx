05ae781b4daac94a0cce305064453d81c0228562
ahsanaaxioned <ahsana@axioned.com>
2023-08-17 01:58:49 +0530

    Added: onclick event added on list

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 
 const Todos = () => {
-  const [inputVal, setInputVal] = useState(""),
-    [todostask, setTodosTask] = useState([]),
-    [empty, setEmpty] = useState(false);
+   const [inputVal, setInputVal] = useState(""),
+    storedTodos = localStorage.getItem("todosItems"),
+    initialTodos = storedTodos ? JSON.parse(storedTodos) : [],
+    [todostask, setTodosTask] = useState(initialTodos),
+    [empty, setEmpty] = useState(false),
+    [completed, setCompleted] = useState(false)
 
   const submitEvent = (e) => {
     e.preventDefault();
@@ -32,6 +35,10 @@ const Todos = () => {
     localStorage.setItem("todosItems", JSON.stringify(todostask));
   }, [todostask]);
 
+  const completedTodo = (idx)=>{
+    setCompleted(true);
+  }
+
   return (
     <div className="w-[70%] mx-auto bg-white shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
       <form onSubmit={submitEvent}>
@@ -52,19 +59,19 @@ const Todos = () => {
       <div>
         <ul>
           {todostask.map((value, idx) => (
-            <li key={idx} className="common relative icon completed">
-              {value}
-            </li>
+             <li key={idx} className = {`${completed ? 'line-through completed' : ''} common relative icon cursor-pointer`} onClick={()=>completedTodo(idx)}>
+             {value}
+           </li>
           ))}
         </ul>
         <div className="flex justify-between px-[15px] items-center">
           <span>{todostask.length} item{todostask.length !== 1 ? "s" : ""} left</span>
           <ul className="flex justify-between py-3 basis-[45%] text-black gap-2">
-            <li className="basis-[20%] border-[1px] border-[#EFD5D5] block text-center">
+          <li className="basis-[20%] border-[1px] border-[#EFD5D5] block text-center cursor-pointer">
               All
             </li>
-            <li className="basis-[38%] block text-center">Active</li>
-            <li className="basis-[38%] block text-center">Completed</li>
+            <li className="basis-[38%] block text-center cursor-pointer">Active</li>
+            <li className={`basis-[38%] block text-center cursor-pointer`}>Completed</li>
           </ul>
           <button>Clear completed</button>
         </div>
